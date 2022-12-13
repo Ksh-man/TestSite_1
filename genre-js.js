872ac885c2b74adcc0ddc6cd9cce8674dd59538f
Ksh <kshkamblemhje@gmail.com>
2022-12-13 12:51:26 +0530

    Initial commit

---
+++ genre-js.js
@@ -0,0 +1,64 @@
+document.getElementById("b1").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b2").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+    
+    document.getElementById("b3").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b4").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b5").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b6").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b7").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b8").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b9").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b10").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b11").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b12").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
+
+    document.getElementById("b13").onclick = function () 
+    {
+        location.href = "You Searched for.html";
+    };
---
+++ yousearchedfor-js.js
@@ -0,0 +1,24 @@
+        document.getElementById("v1").onclick = function()
+        {
+            location.href="video-page.html";
+        };
+        document.getElementById("v2").onclick = function()
+        {
+            location.href="video-page.html";
+        };
+        document.getElementById("v3").onclick = function()
+        {
+            location.href="video-page.html";
+        };
+        document.getElementById("v4").onclick = function()
+        {
+            location.href="video-page.html";
+        };
+        document.getElementById("v5").onclick = function()
+        {
+            location.href="video-page.html";
+        };
+        document.getElementById("v6").onclick = function()
+        {
+            location.href="video-page.html";
+        };
